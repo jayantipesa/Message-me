4b0f52ad5b61b742ad6e8b412278832a82435503
Jayanti Pesa <jayanti.pesa@gmail.com>
2020-09-16 16:06:12 +0530

    Updated message scope and final commit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,9 +17,26 @@
 //= require jquery
 //= require semantic-ui
 
+submit_message = function() {
+    $('#message_body').on('keydown', function(e) {
+      if (e.keyCode == 13) {
+        $('button').click();
+        e.target.value = "";
+      };
+    });
+  };
+scroll_bottom = function() {
+    if ($('#messages').length > 0) {
+      $('#messages').scrollTop($('#messages')[0].scrollHeight);
+    }
+  }
+
 $(document).on('turbolinks:load', function() {
     $('.ui.dropdown').dropdown();
     $('.message .close').on('click', function() {
         $(this).closest('.message').transition('fade');
     });
+    submit_message();
+    scroll_bottom();
 })
+
